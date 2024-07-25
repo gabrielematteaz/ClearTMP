9e8096cb7a9d1089000b8f32e4fddb81112392a1
Matteaz <79192782+gabrielematteaz@users.noreply.github.com>
2024-07-25 12:59:29 +0200

    updated "matteaz" library

--- main.cc
+++ main.cc
@@ -1,17 +1,14 @@
-#include < iostream >
 #include "filesystem.h"
 
 namespace clear_TMP
 {
 	using wstring = std::basic_string < wchar_t, std::char_traits < wchar_t >, matteaz::allocator < wchar_t > >;
 
-	matteaz::allocator < std::byte > allocator;
-
 	void clear_files(wstring &directory)
 	{
 		auto length = directory.length();
 
-		for (matteaz::directory_iterator iterator(directory.c_str(), allocator); iterator; ++iterator) {
+		for (matteaz::directory_iterator iterator(directory.c_str(), directory.get_allocator()); iterator; ++iterator) {
 			auto entry = *iterator;
 
 			if (entry.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
@@ -20,7 +17,6 @@ namespace clear_TMP
 				directory.replace(length, directory.length(), L"\\");
 				directory.append(entry.cFileName);
 				clear_files(directory);
-				RemoveDirectoryW(directory.c_str());
 			}
 			else {
 				directory.replace(length, directory.length(), L"\\");
@@ -28,28 +24,29 @@ namespace clear_TMP
 				DeleteFileW(directory.c_str());
 			}
 		}
+
+		directory.resize(length);
+		RemoveDirectoryW(directory.c_str());
 	}
 }
 
-int WINAPI wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE previous_instance, _In_ LPWSTR command_line, _In_ int show_command)
+int WINAPI wWinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPWSTR, _In_ int)
 {
 	HRESULT code = S_OK;
 
 	try {
-		auto size = GetTempPath2W(0, nullptr);
-		clear_TMP::wstring directory(size, L'\0', clear_TMP::allocator);
-		auto length = GetTempPath2W(static_cast < DWORD > (directory.capacity()), directory.data());
+		clear_TMP::wstring directory(MAX_PATH + 1, L'\0');
+		SIZE_T result = GetTempPath2W(static_cast < DWORD > (directory.capacity()), directory.data());
 
-		if (length == 0) throw matteaz::HRESULT_error(HRESULT_FROM_WIN32(GetLastError()), L"GetTempPath2W failed");
+		if (result == 0) throw matteaz::DWORD_error(L"GetTempPath2W failed");
 
-		--length;
-		directory.resize(length);
+		directory.resize(result - 1);
 		clear_TMP::clear_files(directory);
 	}
-	catch (const matteaz::HRESULT_error &error) {
-		code = error.code_;
+	catch (const matteaz::DWORD_error &error) {
+		code = HRESULT_FROM_WIN32(error.code);
 	}
-	catch (const matteaz::exception&) {
+	catch (const matteaz::basic_exception &) {
 		code = E_FAIL;
 	}
 
